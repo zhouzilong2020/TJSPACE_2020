53bd1775404fd6b77457dfa90df0f7fc9c7702d6
Fitz <1227678132@qq.com>
2020-07-31 17:17:47 +0800

    update on DBclass

--- back-end/TJSpace/TJSpace/DBModel/Major.cs
+++ back-end/TJSpace/TJSpace/DBModel/Major.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using MySql.Data.EntityFrameworkCore.DataAnnotations;
+using Newtonsoft.Json;
 
 namespace TJSpace.DBModel
 {
@@ -10,6 +12,18 @@ namespace TJSpace.DBModel
 
     public class Major
     {
-        
+        //专业编号
+        [JsonProperty("majorid")]
+        [Required]
+        [Column("major_id")]
+        [StringLength(maximumLength: 10)]
+        public string MajorId { get; set; }
+
+        //专业名
+        [JsonProperty("majorname")]
+        [Required]
+        [Column("major_name")]
+        [StringLength(maximumLength: 20)]
+        public string MajorName { get; set; }
     }
 }
--- back-end/TJSpace/TJSpace/DBModel/Reply.cs
+++ back-end/TJSpace/TJSpace/DBModel/Reply.cs
@@ -1,10 +1,43 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MySql.Data.EntityFrameworkCore.DataAnnotations;
+using Newtonsoft.Json;
+
 namespace TJSpace.DBModel
 {
+    [MySqlCharset("utf8mb4")] //字符集
+    [MySqlCollation("utf8mb4_general_ci")] //排序规则
+    [Table("reply", Schema = "tjspace")]
+
     public class Reply
     {
-        public Reply()
-        {
-        }
+        //贴子编号
+        [JsonProperty("replyid")]
+        [Required]
+        [Column("reply_id")]
+        [StringLength(maximumLength: 10)]
+        public string ReplyId { get; set; }
+
+        //贴子内容
+        [JsonProperty("content")]
+        [Required]
+        [Column("content")]
+        [StringLength(maximumLength: 200)]
+        public string Content { get; set; }
+
+        //用户编号
+        [JsonProperty("userid")]
+        [Required]
+        [Column("user_id")]
+        [StringLength(maximumLength: 20)]
+        public string UserId { get; set; }
+
+        //贴子编号
+        [JsonProperty("postid")]
+        [Required]
+        [Column("post_id")]
+        [StringLength(maximumLength: 20)]
+        public string PostId { get; set; }
     }
 }
