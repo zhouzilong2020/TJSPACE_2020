af3e02450104ee85221342b3460271e882278f93
tianfangreal <68943083+tianfangreal@users.noreply.github.com>
2020-08-08 09:14:29 +0800

    Update Comment.cs

--- back-end/TJSpace/TJSpace/DBModel/Comment.cs
+++ back-end/TJSpace/TJSpace/DBModel/Comment.cs
@@ -18,96 +18,29 @@ namespace TJSpace.DBModel
         [JsonProperty("commentid")]
         [Required]
         [Column("comment_id")]
-        [StringLength(maximumLength: 20)]
+        [StringLength(maximumLength: 50)]
         public string CommentId { get; set; }
 
         //内容
         [JsonProperty("content")]
         [Required]
         [Column("content")]
-        [StringLength(maximumLength: 20)]
+        [StringLength(maximumLength: 200)]
         public string Content { get; set; }
 
-        //发布时间
-        [JsonProperty("date")]
-        [Required]
-        [Column("date", TypeName = "date")]
-        public DateTime Date { get; set; }
-
-        //课程总体评分
-        [JsonProperty("overall")]
-        [Required]
-        [Column("overall", TypeName = "int(11)")]
-        public int Overrall { get; set; }
-
-        //对相关教师的评分
-        [JsonProperty("instructor")]
-        [Required]
-        [Column("instructor", TypeName = "int(11)")]
-        public int Instructor { get; set; }
-
-        //课程的难易程度量化评分
-        [JsonProperty("grading")]
-        [Required]
-        [Column("grading", TypeName = "int(11)")]
-        public int Grading { get; set; }
-
-        //工作量量化评分
-        [JsonProperty("workload")]
-        [Required]
-        [Column("workload", TypeName = "int(11)")]
-        public int Workload { get; set; }
-
-        //点赞数
-        [JsonProperty("usefulnum")]
-        [Column("useful_num",TypeName ="int(11)")]
-        public int UsefulNum { get; set; }
-
-        //点踩数
-        [JsonProperty("uselessnum")]
-        [Column("useless_num", TypeName = "int(11)")]
-        public int UselessNum { get; set; }
-
-        //用户id
-        [JsonProperty("userid")]
-        [Required]
-        [Column("user_id")]
-        [StringLength(maximumLength: 20)]
-        public string UserId { get; set; }
-
-        //是否匿名
-        [JsonProperty("anomymous")]
-        [Required]
-        [Column("anonymous",TypeName ="int(11)")]
-        public int Anonymous { get; set; }
-
-        //课程编号
-        [JsonProperty("courseid")]
-        [Required]
-        [Column("course_id")]
-        [StringLength(maximumLength:20)]
-        public string CourseId { get; set; }
-
-        //授课老师编号
-        [JsonProperty("teacherid")]
-        [Required]
-        [Column("teacher_id")]
-        [StringLength(maximumLength:10)]
-        public string TeacherId { get; set; }
-
-        //对教学水平的文字评价
+        //教学水平
         [JsonProperty("teaching")]
         [Column("teaching")]
-        [StringLength(maximumLength:200)]
+        [StringLength(maximumLength: 200)]
         public string Teaching { get; set; }
 
-        //对给分情况的文字评价
+        //给分情况
         [JsonProperty("grade")]
         [Column("grade")]
         [StringLength(maximumLength: 200)]
         public string Grade { get; set; }
 
-        //对课程作业的文字评价
+        //教学水平
         [JsonProperty("homework")]
         [Column("homework")]
         [StringLength(maximumLength: 200)]
@@ -125,31 +58,31 @@ namespace TJSpace.DBModel
         [Column("final", TypeName = "int(11)")]
         public int Final { get; set; }
 
-        //有无小测验
+        //有无小测
         [JsonProperty("quiz")]
         [Required]
         [Column("quiz", TypeName = "int(11)")]
         public int Quiz { get; set; }
 
-        //有无课后作业
+        //有无作业
         [JsonProperty("assignment")]
         [Required]
         [Column("assignment", TypeName = "int(11)")]
         public int Assignment { get; set; }
 
-        //有无课程论文
+        //有无论文
         [JsonProperty("essay")]
         [Required]
         [Column("essay", TypeName = "int(11)")]
         public int Essay { get; set; }
 
-        //有无课程项目
+        //有无项目
         [JsonProperty("project")]
         [Required]
         [Column("project", TypeName = "int(11)")]
         public int Project { get; set; }
 
-        //有无考勤
+        //有无点名
         [JsonProperty("attendence")]
         [Required]
         [Column("attendence", TypeName = "int(11)")]
@@ -161,11 +94,82 @@ namespace TJSpace.DBModel
         [Column("reading", TypeName = "int(11)")]
         public int Reading { get; set; }
 
-        //有无个人展示
+        //有无点名
         [JsonProperty("presentation")]
         [Required]
         [Column("presentation", TypeName = "int(11)")]
         public int Presentation { get; set; }
+
+        //课程总体评分
+        [JsonProperty("overall")]
+        [Required]
+        [Column("overall", TypeName = "int(11)")]
+        public int Overrall { get; set; }
+
+        //对相关教师的评分
+        [JsonProperty("instructor")]
+        [Required]
+        [Column("instructor", TypeName = "int(11)")]
+        public int Instructor { get; set; }
+
+        //课程的给分情况的评分
+        [JsonProperty("grading")]
+        [Required]
+        [Column("grading", TypeName = "int(11)")]
+        public int Grading { get; set; }
+
+        //工作量量化评分
+        [JsonProperty("workload")]
+        [Required]
+        [Column("workload", TypeName = "int(11)")]
+        public int Workload { get; set; }
+
+        //发布时间
+        [JsonProperty("date")]
+        [Required]
+        [Column("date", TypeName = "date")]
+        public DateTime Date { get; set; }
+
+        //点赞数
+        [JsonProperty("usefulnum")]
+        [Column("useful_num",TypeName ="int(11)")]
+        public int UsefulNum { get; set; }
+
+        //点踩数
+        [JsonProperty("uselessnum")]
+        [Column("useless_num", TypeName = "int(11)")]
+        public int UselessNum { get; set; }
+
+        //用户id
+        [JsonProperty("userid")]
+        [Required]
+        [Column("user_id")]
+        [StringLength(maximumLength: 50)]
+        public string UserId { get; set; }
+
+        //是否匿名
+        [JsonProperty("anomymous")]
+        [Required]
+        [Column("anonymous",TypeName ="int(11)")]
+        public int Anonymous { get; set; }
+
+        //课程编号
+        [JsonProperty("courseid")]
+        [Required]
+        [Column("course_id")]
+        [StringLength(maximumLength:50)]
+        public string CourseId { get; set; }
+
+        //授课老师编号
+        [JsonProperty("teacherid")]
+        [Required]
+        [Column("teacher_id")]
+        [StringLength(maximumLength:50)]
+        public string TeacherId { get; set; }
+
+
+
+
 
     }
 }
