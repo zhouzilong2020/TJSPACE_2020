794d467b2749af02c59117041b789c07b900ee4a
tianfangreal <68943083+tianfangreal@users.noreply.github.com>
2020-08-08 09:19:21 +0800

    Update Post.cs

--- back-end/TJSpace/TJSpace/DBModel/Post.cs
+++ back-end/TJSpace/TJSpace/DBModel/Post.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using MySql.Data.EntityFrameworkCore.DataAnnotations;
@@ -8,30 +8,45 @@ namespace TJSpace.DBModel
 {
     [MySqlCharset("utf8mb4")] //字符集
     [MySqlCollation("utf8mb4_general_ci")] //排序规则
-    [Table("post", Schema = "tjspace")]
+    [Table("mark", Schema = "tjspace")]
 
-    public class Post
+    public class Mark
     {
+        //用户编号
+        [Key]
+        [JsonProperty("userid")]
+        [Required]
+        [Column("user_id")]
+        [StringLength(maximumLength: 50)]
+        public string UserId { get; set; }
+
+        //回复编号
+        [Key]
+        [JsonProperty("replyid")]
+        [Required]
+        [Column("reply_id")]
+        [StringLength(maximumLength:50)]
+        public string ReplyId { get; set; }
+
         //贴子编号
         [Key]
         [JsonProperty("postid")]
         [Required]
         [Column("post_id")]
-        [StringLength(maximumLength: 20)]
+        [StringLength(maximumLength: 50)]
         public string PostId { get; set; }
 
-        //贴子内容
-        [JsonProperty("content")]
+        //评价种类
+        [JsonProperty("type")]
         [Required]
-        [Column("content")]
-        [StringLength(maximumLength: 200)]
-        public string Content { get; set; }
+        [Column("type",TypeName ="int(11)")]
+        public int Type { get; set; }
 
-        //用户编号
-        [JsonProperty("userid")]
+        //发布时间
+        [JsonProperty("date")]
         [Required]
-        [Column("user_id")]
-        [StringLength(maximumLength: 20)]
-        public string UserId { get; set; }
+        [Column("date", TypeName = "date")]
+        public DateTime Date { get; set; }
+
     }
 }
