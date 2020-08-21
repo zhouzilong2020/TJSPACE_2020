87090df5909fd93a7d4414ff1d7f9afed7b378bd
tianfangreal <68943083+tianfangreal@users.noreply.github.com>
2020-08-21 14:39:09 +0800

    Create Entity.cs

---
+++ back-end/TJSpace/TJSpace/DBModel/Entity.cs
@@ -0,0 +1,25 @@
+using System;
+namespace TJSpace.DBModel
+{
+    public class SearchCourseReturn
+    {
+        public string TeacherName { get; set; }
+        public int Semester { get; set; }
+        public int Year { get; set; }
+    }
+
+    public class SearchPostReturn
+    {
+        public string PostId { get; set; }//帖子id
+        public string Title { get; set; }//帖子名称
+        public DateTime Date { get; set; }//帖子标题
+        public string Name { get; set; }//发帖人
+    }
+
+    public class ShowPostReturn
+    {
+        public string ReplyId { get; set; }
+        public int floor { get; set; }
+        public int type { get; set; }
+    }
+}
