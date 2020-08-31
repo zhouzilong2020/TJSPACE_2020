63680f87e58568dd950dc20f8e2024f92eb08a9a
zhouzilong2020 <529620861@qq.com>
2020-08-31 12:14:17 +0800

    userService API

--- font-end/tjspace/src/services/userService.js
+++ font-end/tjspace/src/services/userService.js
@@ -1,8 +1,10 @@
 // 远程登录用户！哈哈
 import axios from "axios";
 import {URL} from './config'
+
 /**
- * 登录用户
+ * 登录用户的API接口，如果成功返回token以及userID，并将token，userid保存在浏览器的localstorage中；
+ * @param {Object} payload payload需要传入email，password
  */
 export async function loginUser(payload) {
     console.log("in userSevice", payload)
@@ -17,14 +19,24 @@ export async function loginUser(payload) {
     return resp.data;
 }
 
+/**
+ * 使用浏览器保存的userID和token来获取我是谁，如果token过期则清除本地保存的信息
+ */
 export async function whoAmI(){
 
 }
 
+/**
+ * 退出登录，删除本地保存的token和userID信息
+ */
 export async function logoutUser(){
 
 }
 
-export async function registerUser(){
+/**
+ * 注册用户API，如果成功返回token以及userID，并将token，userid保存在浏览器的localstorage中；
+ * @param {Object} payload payload需要传入 email，password，nickname，以及邮箱的相关验证码
+ */
+export async function registerUser(payload){
     
 }
--- font-end/tjspace/src/store/userInfo.js
+++ font-end/tjspace/src/store/userInfo.js
@@ -1,6 +1,8 @@
-// 登录用户的仓库数据
 import {loginUser, logoutUser} from "../services/userService"
 
+/**
+ * 用户登录的数据仓库
+ */
 export default{
     namespaced: true,
     state: {
@@ -8,28 +10,45 @@ export default{
         isLoading: false,
     },
     mutations: {
+        /**
+         * 修改个人信息
+         * @param {*} state 指向本仓库的state
+         * @param {Object} payload payload以object形式传入，包括nickname，userid，email等
+         */
         setUserInfo(state, payload){
             state.userInfo = payload;
         },
-        setToken(state, payload) {
-            state.token = payload;
-        },
+        /**
+         * 修改是否正在加载
+         * @param {*} state 指向本仓库的state
+         * @param {boolean} payload payload传入true or false
+         */
         setIsLoading(state, payload){
             state.isLoading = payload;
         },
     },
  
     actions: {
+        /**
+         * 登录用户
+         * @param {*} context 
+         * @param {Object} payload payload传入email，password
+         */
         async loginUser(context, payload){
             context.commit("setIsLoading", true);     
             var resp = await loginUser(payload);
             console.log(resp);
             if(resp.status){
-                let token = "Bearer " + resp.data;
-                context.commit("setToken", token);      
+                context.commit("setUserInfo", {
+                    email : payload.email,
+                });
             }
             context.commit("setIsLoading", false);
         },
+        /**
+         * 推出登录，将数据仓库中的用户信息置空
+         * @param {*} context 
+         */
         async logoutUser(context){
             context.commit("setIsLoading", true);   
             var resp = await logoutUser()
