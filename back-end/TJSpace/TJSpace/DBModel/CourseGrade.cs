701fbd576613d63fb8fe0e95955fbfe24b8bbd8d
DESKTOP-SAJQTAO\matianfang <582415714@qq.com>
2020-09-15 22:20:12 +0800

    no message

---
+++ back-end/TJSpace/TJSpace/DBModel/CourseGrade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MySql.Data.EntityFrameworkCore.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace TJSpace.DBModel
+{
+    [MySqlCharset("utf8mb4")] //字符集
+    [MySqlCollation("utf8mb4_general_ci")] //排序规则
+    [Table("course_grade", Schema = "tjspace")]
+    public class CourseGrade
+    {
+        //教师编号
+        [Key]
+        [JsonProperty("teacherid")]
+        [Required]
+        [Column("teacher_id")]
+        [StringLength(maximumLength: 64)]
+        public string TeacherId { get; set; }
+
+        //课程编号
+        [Key]
+        [JsonProperty("courseid")]
+        [Required]
+        [Column("course_id")]
+        [StringLength(maximumLength: 50)]
+        public string CourseId { get; set; }
+
+        //平均分
+        [JsonProperty("avgscore")]
+        [Column("avg_score", TypeName = "double")]
+        public double AvgScore { get; set; }
+    }
+}
